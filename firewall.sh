09c13a133b41447738fa6caa14ae576d48d86bf4
tinsae-ghilay <85639930+tinsae-ghilay@users.noreply.github.com>
2024-01-21 20:03:02 +0100

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -3,7 +3,7 @@ cd
 echo "Are you sure you want to change Firewall settings?"
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
-echo reseting iptables rules  :: :: :: :: !
+echo reseting iptables rules  :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: !
 if
 # restore rules to default
 echo reseting rules to default
@@ -11,7 +11,7 @@ iptables-restore < /etc/iptables/empty.rules
 then
 
 # try connection
-echo trying network connection with default  :: :: :: :: !
+echo trying network connection with default  :: :: :: :: :: :: :: :: :: :: :: !
 ping -c 3 archlinux.org
 
 #create neccesary chains
@@ -20,42 +20,42 @@ iptables -N UDP
 
 # forwarding chain
 # drop forwarding
-echo droping forwarding chain  :: :: :: :: !
+echo droping forwarding chain   :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: !
 iptables -P FORWARD DROP
 
 # outgoing chain
 # accept outgoing
-echo acceping output  :: :: :: :: !
+echo acceping output : :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: !
 iptables -P OUTPUT ACCEPT
 
 # the input chain
 # drop outgoing
-echo droping input chain  :: :: :: :: !
+echo droping input chain :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: !
 iptables -P INPUT DROP
 
 # try connection
-echo trying network after droping input chain  :: :: :: :: !
+echo trying network after droping input chain :: :: :: :: :: :: :: :: :: :: !
 ping -c 3 archlinux.org
 
 # exeptions to input rule
-echo adding exceptions to input chain rule  :: :: :: :: !
+echo adding exceptions to input chain rule  :: :: :: :: :: :: :: : :: :: :: !
 if
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 then
-echo related and established are accepted  :: :: :: :: !
+echo related and established are accepted  :: :: :: :: :: :: :: :: :: :: :: !
 fi
 # accept traffic from loopback(necesary for printers)
 if
 iptables -A INPUT -i lo -j ACCEPT
 then
-echo loopback accepted  :: :: :: :: !
+echo loopback accepted  :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: !
 fi
 
 # drop invalid connections
 if
 iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
 then
-echo rules for invalid connections set  :: :: :: :: !
+echo rules for invalid connections set  :: :: :: :: :: :: :: :: :: :: ::  !
 fi
 
 # next rule is for accepting a ping (do I need it? for a personal PC)
@@ -63,18 +63,18 @@ fi
 
 # attach TCP and UDP chains to INPUT chain to handle all incoming connections
 # Once accepted by TCP or UDP, it is handled by RELATED/ESTABLISHED traffic rule
-echo attaching udp and tcp chains to input  :: :: :: :: !
+echo attaching udp and tcp chains to input  :: :: :: :: :: :: :: :: :: :: !
 iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
 iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
 
 # reject TCP connections with TCP RESET packets & UDP streams
-echo rejecting icmp inputs  :: :: :: :: !
+echo rejecting icmp inputs  :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: !
 iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
 iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
 iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
 
 # save rules 
-echo saving rules to /etc/iptables/iptables.rules :: :: :: :: !
+echo saving rules to /etc/iptables/iptables.rules :: :: :: :: :: : :: :: !
 iptables-save -f /etc/iptables/iptables.rules
 
 # To accept incoming TCP connections on port 80 for a web server
@@ -86,10 +86,10 @@ iptables-save -f /etc/iptables/iptables.rules
 # ssh on port 22
 # iptables -A TCP -p tcp --dport 22 -j ACCEPT
 
-echo enabling and starting iptables :: :: :: :: !
+echo enabling and starting iptables :: :: :: :: :: :: :: :: :: :: :: :: !
 systemctl enable iptables && systemctl start iptables
 systemctl status iptables
-echo DONE :: :: :: !!!!
+echo DONE :: :: ::  :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: !!!!!
 else
 echo iptables not found, please install it and run this script again.
 fi
